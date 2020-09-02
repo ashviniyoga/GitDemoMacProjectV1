c6cbdebd661795bc7ab56e478b881e2dd3f41b0f
mcb <mcb@192.168.0.12>
2020-09-02 13:06:20 -0700

    First commit

---
+++ src/GitDemo.java
@@ -0,0 +1,16 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String os = "Mackk";
+		
+		if(os=="Mac") {
+			System.out.println("The Operating sys is MAC");
+		}
+		else
+			System.out.println("The Operating sys is Windows");
+	}
+
+}
